87bd6373301c5ded8cfc541c6c61d14f82178de5
Ivory <IvoryEC@gmail.com>
2020-02-28 15:44:12 -0500

    Keep using existing image for genome assembly module.

--- src/biolockj/module/assembly/GenomeAssembly.java
+++ src/biolockj/module/assembly/GenomeAssembly.java
@@ -139,6 +139,11 @@ public class GenomeAssembly extends SeqModuleImpl {
 	public String getDockerImageName() {
 		return "genome_assembly";
 	}
+	
+	@Override
+	public String getDockerImageTag() {
+		return "v1.2.0";
+	}
 
 	/**
 	 * {@link biolockj.Config} exe property for checkm executable: {@value #EXE_CHECKM}
