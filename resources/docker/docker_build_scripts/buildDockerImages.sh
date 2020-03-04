1c18af9f6bdc370d215779dcd63c8f4ace607269
Ivory <IvoryEC@gmail.com>
2020-03-04 14:42:57 -0500

    After a successful image build, tag image with anticipated release version, current version, and 'latest'

--- resources/docker/docker_build_scripts/buildDockerImages.sh
+++ resources/docker/docker_build_scripts/buildDockerImages.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-cd ${BLJ}
-VER=$(cat .version)
+VER=$(cat ${BLJ}/.version)
 TAG=${VER//-*}
-DIR=resources/docker/dockerfiles
+DIR=${BLJ}/resources/docker/dockerfiles
 
-FILES=$(ls $DIR)
+FILES=$(cat ${BLJ}/resources/docker/docker_build_scripts/buildOrder.txt)
 for f in $FILES; do
 	name=${f//.Dockerfile}
+	echo "============================"
 	echo $name
-	docker build -t biolockjdevteam/${name}:${TAG} . -f ${DIR}/${name}.Dockerfile || echo "FAILURE: $name"
+	docker build -t biolockjdevteam/${name}:${TAG} ${BLJ} -f ${DIR}/${name}.Dockerfile && \
+	docker tag biolockjdevteam/${name}:${TAG} biolockjdevteam/${name}:${VER} && \
+	docker tag biolockjdevteam/${name}:${TAG} biolockjdevteam/${name}:latest && echo "SUCCESS: $name" 1>&2 || echo "FAILURE: $name" 1>&2
 done
